1c3e219b7876e27f0fc704a4cb45a22925ca8887
Mohit Lahoti <engtechno25@gmail.com>
2025-01-05 19:21:38 +0530

    Modify models.py for storing hashed password

--- authService/models.py
+++ authService/models.py
@@ -4,9 +4,16 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     id = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
+    first_name = models.CharField(max_length=255, blank=False, null=False)
+    last_name = models.CharField(max_length=255, blank=False, null=False)
     email = models.EmailField(unique=True, blank=False, null=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.username
+
+    def save(self, *args, **kwargs):
+        if self._state.adding and self.password and not self.password.startswith(('pbkdf2_sha256$', 'bcrypt$', 'argon2')):
+            self.set_password(self.password)
+        super().save(*args, **kwargs)
