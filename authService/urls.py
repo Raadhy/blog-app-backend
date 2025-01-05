b90017734866d2922a9c31de707a04248b66c577
Mohit Lahoti <engtechno25@gmail.com>
2025-01-05 19:47:42 +0530

    Create login routes with views

--- authService/urls.py
+++ authService/urls.py
@@ -2,5 +2,6 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('register', views.UserRegistration.as_view())
+    path('register', views.UserRegistration.as_view()),
+    path('login', views.UserLogin.as_view())
 ]
--- authService/views.py
+++ authService/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
@@ -13,3 +13,15 @@ class UserRegistration(APIView):
             # token = get_tokens_for_user(user=user)
             return Response({"msg": "Registration Successful..."}, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class UserLogin(APIView):
+    def post(self, request):
+        email = request.data['email']
+        password = request.data['password']
+        user = authenticate(username=email, password=password)
+        if user is not None:
+            return Response({"msg": "Login Successful...", "user": {
+                "email": email,
+                "id": user.id
+            }}, status=status.HTTP_200_OK)
+        return Response({"error": "Invalid Credentials"}, status=status.HTTP_401_UNAUTHORIZED)
