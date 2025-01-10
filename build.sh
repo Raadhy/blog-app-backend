0b44c5e80bdcadb207ba5e247692f4ba61ffb774
Mohit Lahoti <engtechno25@gmail.com>
2025-01-10 23:41:47 +0530

    Update build.sh from python to python3.12

--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 echo "Updating pip..."
-python -m pip install -U pip
+python3.12 -m pip install -U pip
 
 # Install dependencies
 echo "Installing project dependencies..."
-python -m pip install -r requirements.txt
+python3.12 -m pip install -r requirements.txt
 
 # Collect static files
 echo "Collecting static files..."
-python manage.py collectstatic --noinput --clear --output staticfiles_build
+python3.12 manage.py collectstatic --noinput --clear --output staticfiles_build
