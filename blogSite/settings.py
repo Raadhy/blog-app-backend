185a479b73d932e90022d6af65ceac09cb39d68c
Mohit Lahoti <engtechno25@gmail.com>
2025-01-10 21:52:25 +0530

    Create static folder

--- blogSite/settings.py
+++ blogSite/settings.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 from urllib.parse import urlparse
 
@@ -104,6 +105,8 @@ USE_I18N = True
 
 USE_TZ = True
 
-STATIC_URL = 'static/'
+STATIC_URL = "static/"
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles_build", "static")
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
