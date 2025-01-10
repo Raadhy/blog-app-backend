07ff17712a4746ea0332991277e36e6752172474
Mohit Lahoti <engtechno25@gmail.com>
2025-01-10 23:46:48 +0530

    remove --output from build.sh

--- build.sh
+++ build.sh
@@ -9,4 +9,4 @@ python3.12 -m pip install -r requirements.txt
 
 # Collect static files
 echo "Collecting static files..."
-python3.12 manage.py collectstatic --noinput --clear staticfiles_build
+python3.12 manage.py collectstatic --noinput --clear
