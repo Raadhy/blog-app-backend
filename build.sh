757fa2baaf09c75421785354ce1f75cf66b9ee5b
Mohit Lahoti <engtechno25@gmail.com>
2025-01-11 14:53:46 +0530

    Whitenoise and corsheaders installation

--- build.sh
+++ build.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
 echo "Updating pip..."
-python3.12 -m pip install -U pip
+python3.12 pip install -U pip
 
 # Install dependencies
+
 echo "Installing project dependencies..."
 python3.12 -m pip install -r requirements.txt
 
-# Collect static files
-echo "Collecting static files..."
+#Install whitenoise
+echo "Installing white noise..."
+python3.12 manage.py whitenoise
+# Collect staticfiles
+echo "Collect static..."
 python3.12 manage.py collectstatic --noinput --clear
